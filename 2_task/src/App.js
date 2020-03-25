032049eb5f4c752ad728c43a9d7c406348feafd8
oona-k <59806531+oona-k@users.noreply.github.com>
2020-03-25 13:27:23 +0200

    created Person component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Person from './Person/Person'; /* No need to type Person.js. If any other than .js, the ending is also needed */
 
 const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Person />
     </div>
   );
 };
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Person.css';
+
+const Person = () => {
+return(
+    <div className="card"><p>Hello, my name is Oona and I am 29 years old.</p></div>
+);
+};
+
+export default Person;
