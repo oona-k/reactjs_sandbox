7418b18bfd43027518b1c932d221aeb3685ad0e0
oona-k <59806531+oona-k@users.noreply.github.com>
2020-03-30 11:37:19 +0300

    Added buttons for increasing, decreasing and resetting amount of likes

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
     likes: 0
   };
-  render () {
+
+addLike = () => {
+  console.log('test');
+  this.setState({ likes: this.state.likes + 1});
+};
+
+removeLike = () => {
+  this.setState({ likes: this.state.likes - 1});
+};
+
+reset = () => {
+  this.setState({ likes: this.state.likes = 0});
+};
+
+  render() {
     return (
       <div>
-      <h1>Total likes: {this.state.likes}</h1>
+        <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.addLike}>Like</button>
+        <button onClick={this.removeLike}>Dislike</button>
+        <button onClick={this.reset}>Reset</button>
+
       </div>
     );
   }
