ed608f70e2701478b5dba33d2141149d345c24c1
oona-k <59806531+oona-k@users.noreply.github.com>
2020-04-06 12:29:29 +0300

    first commit

--- 4_task/src/App.js
+++ 4_task/src/App.js
@@ -13,10 +13,15 @@ class App extends Component {
   };
   changeHandler = event => {
     console.log(event.target.name);
-    if (event.target.name === "firstname"){this.setState({ firstname: event.target.value });}
-    else if (event.target.name === "lastname"){this.setState({ lastname: event.target.value });}
-    else if (event.target.name === "phone"){this.setState({ phone: event.target.value });}
-    else {this.setState({ message: event.target.value });}
+    if (event.target.name === "firstname") {
+      this.setState({ firstname: event.target.value });
+    } else if (event.target.name === "lastname") {
+      this.setState({ lastname: event.target.value });
+    } else if (event.target.name === "phone") {
+      this.setState({ phone: event.target.value });
+    } else {
+      this.setState({ message: event.target.value });
+    }
   };
   render() {
     return (
