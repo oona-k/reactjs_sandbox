b7f23b1e96f573f36ca32d3f98c8d69696788ad4
oona-k <59806531+oona-k@users.noreply.github.com>
2020-04-06 13:33:54 +0300

    Added circles and update score

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import Circle from './Circle/Circle';
 
 class App extends Component {
+  state = {
+    score: 0,
+  };
+
+  clickHandler = (circleID) => {
+console.log('Clicked', circleID);
+this.setState({
+  score: this.state.score + 1
+})
+  };
   render() {
     return (
       <div>
-        
+        <h1>Speedtest</h1>
+        <p>Your score: {this.state.score}</p>
+        <main>
+        <Circle click={this.clickHandler.bind(this, 1)}/>
+        <Circle click={this.clickHandler.bind(this, 2)}/>
+        <Circle click={this.clickHandler.bind(this, 3)}/>
+        <Circle click={this.clickHandler.bind(this, 4)}/>
+        </main>
+        <div>
+        <button>Start game</button>
+        <button>End game</button>
+        </div>
       </div>
     );
   }
---
+++ 5_task/src/Circle/Circle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import "./Circle.css"
+
+const Circle = props => {
+    return( <div className="circle" onClick={props.click}></div>);
+}
+export default Circle;
